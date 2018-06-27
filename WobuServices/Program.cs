237e83b0d306a280146afae674d0d71b97ff90db
Ian Nastajus <ian.nastajus@gmail.com>
2018-06-26 22:00:50 -0400

    lame proto2 partial setup in singular .proto file of <everything>

---
+++ WobuServices/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WobuServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
